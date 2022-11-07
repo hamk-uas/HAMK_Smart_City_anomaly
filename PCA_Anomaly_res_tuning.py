87853c15dd36de8cddb9b66e0161f46de69b1874
Olli Niemitalo <olli4@hamk.fi>
2022-11-07 15:36:53 +0200

    +...res_tuning.py

--- PCA_Anomaly_res_tuning.py
+++ PCA_Anomaly_res_tuning.py
@@ -29,7 +29,6 @@ data = pd.read_csv('3452_building_data_full_year_copy.csv')
 
 
 # Convert the data type of timestamp column to datatime format
-print(data['timestamp'])
 data['date'] = pd.to_datetime(data['timestamp'])
 #del data['timestamp']
 # Select only the temperature data and make it index for OT data
@@ -38,70 +37,58 @@ df_temp = df_temp.set_index('date')
 # Remove the zero value and missing value
 df_tempclean = df_temp[df_temp != 0]
 df_tempclean = df_tempclean.dropna()
-df_tempclean = df_tempclean[df_tempclean.index >= pd.to_datetime("2021-12-01 00:00:00+00:00") and df_tempclean.index <= pd.to_datetime("2022-03-15 00:00:00+00:00")]
+df_tempclean = df_tempclean[(df_tempclean.index >= "2021-12-01") & (df_tempclean.index < "2022-03-16")]
 # Vizualize time series in the graph for each device
-his = df_tempclean.hist(df_tempclean.columns, bins = 25, layout = (8, 8), figsize = (18, 18))
-plt.show()
+#his = df_tempclean.hist(df_tempclean.columns, bins = 25, layout = (8, 8), figsize = (18, 18))
+#plt.show()
 #plt.savefig(his)
 
 # Extract the names of the numerical columns
 names = df_tempclean.columns
 x = df_tempclean
 # Standardize/scale the dataset and apply PCA
-scaler = StandardScaler()
-pca = PCA()
-pipeline = make_pipeline(scaler, pca)
-pipeline.fit(x)
 
-features = range(pca.n_components_)
-_ = plt.figure(figsize=(22, 5))
-_ = plt.bar(features, pca.explained_variance_)
-_ = plt.xlabel('PCA feature')
-_ = plt.ylabel('Variance')
-_ = plt.xticks(features)
-_ = plt.title("Importance of the Principal Components based on variance explained")
+#features = range(pca.n_components_)
+#_ = plt.figure(figsize=(22, 5))
+#_ = plt.bar(features, pca.explained_variance_)
+#_ = plt.xlabel('PCA feature')
+#_ = plt.ylabel('Variance')
+#_ = plt.xticks(features)
+#_ = plt.title("Importance of the Principal Components based on variance explained")
+#plt.show()
+
+n_components_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+threshold_list = np.arange(0.125, 40, 0.125)
+result_table_total_var = np.zeros((len(n_components_list), len(threshold_list)))
+result_table_individual = np.zeros((len(n_components_list), len(threshold_list)))
+for n_components_index, n_components in enumerate(n_components_list):
+    scaler = StandardScaler()
+    pca = PCA(n_components = n_components)
+    pipeline = make_pipeline(scaler, pca)
+    principalComponents = pipeline.fit_transform(x) # Note: this could be done as separate fit and transform steps, using non-anomalous data for fit
+
+    # Reconstruct from the n dimensional scores
+    reconstruct = pipeline.inverse_transform(principalComponents)
+    #The residual is the amount not explained by the first n components
+    scaled_residual = pd.DataFrame(data = scaler.transform(x) - pca.inverse_transform(principalComponents), index = df_tempclean.index, columns=df_tempclean.columns)
+
+    for threshold_index, threshold in enumerate(threshold_list):
+        scaled_residual_var = (scaled_residual ** 2).sum(axis = 1)
+        is_anomaly_based_on_scaled_residual_total_var = scaled_residual_var > threshold
+        is_anomaly_based_on_scaled_residual = (scaled_residual ** 2) > threshold
+        result_table_total_var[n_components_index, threshold_index] = is_anomaly_based_on_scaled_residual_total_var.astype('int').sum().sum()
+        result_table_individual[n_components_index, threshold_index] = is_anomaly_based_on_scaled_residual.astype('int').sum().sum()
+
+#print(result_table_total_var)
+#print(result_table_individual)
+print([n_components_list[0], n_components_list[-1], threshold_list[0], threshold_list[-1]])
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+plt.imshow(result_table_total_var, interpolation='nearest', vmin=0, vmax=10, aspect=(threshold_list[-1]-threshold_list[0])/(n_components_list[-1] - n_components_list[0]), extent=[threshold_list[0], threshold_list[-1], n_components_list[0]-0.5, n_components_list[-1]+0.5]) # , cmap=plt.cm.ocean
+plt.colorbar()
 plt.show()
-
-# Calculate PCA with n components
-scaler = StandardScaler()
-pca = PCA(n_components = 3)
-pipeline = make_pipeline(scaler, pca)
-principalComponents = pipeline.fit_transform(x) # Note: this could be done as separate fit and transform steps, using non-anomalous data for fit
-
-# Reconstruct from the n dimensional scores
-reconstruct = pipeline.inverse_transform(principalComponents)
-#The residual is the amount not explained by the first n components
-scaled_residual = pd.DataFrame(data = scaler.transform(x) - pca.inverse_transform(principalComponents), index = df_tempclean.index, columns=df_tempclean.columns)
-residual = df_tempclean - reconstruct
-
-# Detect outliers based on scaled residual variance summed over principal component features
-scaled_residual_total_var_anomaly_threshold = 5 # Arbitrary threshold
-scaled_residual_sq_anomaly_threshold = 5 # Arbitrary threshold
-scaled_residual_var = (scaled_residual ** 2).sum(axis = 1)
-is_anomaly_based_on_scaled_residual_total_var = scaled_residual_var > scaled_residual_total_var_anomaly_threshold
-is_anomaly_based_on_scaled_residual = (scaled_residual ** 2) > scaled_residual_sq_anomaly_threshold
-
-# Let's plot the outliers from pc1 on top of the device_1783 see where they occured in the time series
-a1 = df_tempclean[is_anomaly_based_on_scaled_residual_total_var] #anomaly
-b1 = residual[is_anomaly_based_on_scaled_residual_total_var] #anomaly
-a2 = df_tempclean[is_anomaly_based_on_scaled_residual] #anomaly
-b2 = residual[is_anomaly_based_on_scaled_residual] #anomaly
-for name in names:
-    _ = plt.figure(figsize = (18,8))
-    #_ = plt.plot(df_tempclean['(\'Temperature_01\', 1392)'], color='blue', label='Normal')
-    _ = plt.plot(df_tempclean[name], color = 'blue', label = 'Feature')
-    #_ = plt.plot(residual['(\'Temperature_01\', 1392)'], color = 'black', label = 'Residual')
-    _ = plt.plot(residual[name], color = 'black', label = 'Residual')
-    _ = plt.plot(a1[name], linestyle = 'none', marker = '.', color = 'orange', markersize = 12, label = 'Anomaly based on all residuals')
-    _ = plt.plot(a2[name], linestyle = 'none', marker = '.', color = 'red', markersize = 8, label = 'Anomaly based on residual')
-    _ = plt.plot(b1[name], linestyle = 'none', marker = '.', color='orange', markersize = 12)
-    _ = plt.plot(b2[name], linestyle = 'none', marker = '.', color='red', markersize = 8)
-    _ = plt.xlabel('Date and Time')
-    _ = plt.ylabel('device Reading')
-    #_ = plt.title('Temperature_01 1392 Anomalies')
-    _ = plt.title(name)
-    _ = plt.legend(loc = 'best')
-    plt.grid()
-    plt.autoscale()
-    plt.show()
-
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+plt.imshow(result_table_individual, interpolation='nearest', vmin=0, vmax=10, aspect=(threshold_list[-1]-threshold_list[0])/(n_components_list[-1] - n_components_list[0]), extent=[threshold_list[0], threshold_list[-1], n_components_list[0]-0.5, n_components_list[-1]+0.5]) # , cmap=plt.cm.ocean
+plt.colorbar()
+plt.show()
